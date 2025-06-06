f47a623cb089451c570f3aec0598ae2a22830d78
Darby Manning <me@darbymanning.com>
2025-06-06 22:03:57 +0100

    ft: Add sprite types vite module

--- src/lib/vite/index.ts
+++ src/lib/vite/index.ts
@@ -1,3 +1,5 @@
 export { prepend_css } from './prepend_css/index.js'
 export { prepend_css as prependCSS } from './prepend_css/index.js'
 export { outwatch } from './outwatch/index.js'
+export { sprite_types } from './sprite_types/index.js'
+export { sprite_types as spriteTypes } from './sprite_types/index.js'
