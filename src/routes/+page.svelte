f8bef1ed9afe86443708c8c9468345ace75988b4
Matt Pilott <matt@pilott.me>
2026-03-12 20:25:30 +0000

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
-	import '$lib/styles/unify.css'
+	import '../app.css'
 	import Overlay from '$lib/svelte/overlay/index.svelte'
 </script>
 
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<Overlay mobile="/mobile.jpg" desktop="/desktop.jpg" />
+
+<Overlay desktop="/desktop.png" />
 
 <style lang="css">
 	h1 {
 		color: red;
-		size: 100px;
+
+		@media (--from-tablet) {
+			color: blue;
+		}
 	}
 </style>
